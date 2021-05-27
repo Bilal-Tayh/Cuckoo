9fdfa3296271bfb3df28cab509a7d86e5576ceb2
bilal tayh <46609468+Bilal-Tayh@users.noreply.github.com>
2021-05-27 15:06:15 +0300

    Add files via upload

--- Cuckoo-montyCarlo1/main1.cpp
+++ Cuckoo-montyCarlo1/main1.cpp
@@ -102,33 +102,277 @@ void getKeysAndWeightsFromFile(string filename, vector<key*> &keys, vector<val*>
 
 int main(int argc, char* argv[])
 {
-    vector<key*> keys;
-    vector<val*> values;
-    vector<int> sizes;
-    vector<string> datasets;
+//     vector<key*> keys;
+//     vector<val*> values;
+//     vector<int> sizes;
+//     vector<string> datasets;
     
-/*
+
+//     getKeysAndWeightsFromFile("../../datasets/UNIV1/mergedAggregatedPktlen_Srcip", keys, values, UNIV1_SIZE);
+//     sizes.push_back(UNIV1_SIZE);
+//     datasets.push_back("univ1");
+//     
+
+
+//     getKeysAndWeightsFromFile("../../datasets/CAIDA16/mergedAggregatedPktlen_Srcip", keys, values, CAIDA16_SIZE);
+//     sizes.push_back(CAIDA16_SIZE);
+//     datasets.push_back("caida");
+
+
+
+//     getKeysAndWeightsFromFile("../../datasets/CAIDA18/mergedAggregatedPktlen_Srcip", keys, values, CAIDA18_SIZE);
+//     sizes.push_back(CAIDA18_SIZE);
+//     datasets.push_back("caida18");
+
+
+    
+    
+    
+    
+    
+    
+    
+/*    
     getKeysAndWeightsFromFile("../../datasets/UNIV1/mergedAggregatedPktlen_Srcip", keys, values, UNIV1_SIZE);
     sizes.push_back(UNIV1_SIZE);
-    datasets.push_back("univ1");*/
+    datasets.push_back("univ1");
+
+    
+    
+    vector<key*>::iterator k_it = keys.begin();
+    key* k = *k_it;
+
+    int size =sizeof(key*);
+    int traceSize = UNIV1_SIZE;
+    ofstream f;
+
+    f.open("univ1_k.dat",ios::binary|ios::out); 
+
+    for(int j=0;j<traceSize;j++){
+        f.write((char *)&k[j],size);
+    }
+
+    f.close();
+    
+    
+    
+    
+    vector<val*>::iterator v_it = values.begin();
+    val* v = *v_it;
+
+    int size1 =sizeof(val*);
+    ofstream ff;
+
+    ff.open("univ1_v.dat",ios::binary|ios::out); 
+
+    for(int j=0;j<traceSize;j++){
+        ff.write((char *)&v[j],size1);
+    }
+
+    ff.close();
+    */
+    
+    
+    
+    
+    
+//##################
+    
+    
+    
+    
+    
+    
+//     getKeysAndWeightsFromFile("../../datasets/CAIDA16/mergedAggregatedPktlen_Srcip", keys, values, CAIDA16_SIZE);
+//     sizes.push_back(CAIDA16_SIZE);
+//     datasets.push_back("caida");
+// 
+// 
+//     
+//     
+//     vector<key*>::iterator k_it = keys.begin();
+//     key* k = *k_it;
+// 
+//     int size =sizeof(key*);
+//     int traceSize = CAIDA16_SIZE;
+//     ofstream f1;
+// 
+//     f1.open("ca16_k.dat",ios::binary|ios::out); 
+// 
+//     for(int j=0;j<traceSize;j++){
+//         f1.write((char *)&k[j],size);
+//     }
+// 
+//     f1.close();
+//     
+//     
+//     
+//     
+//     vector<val*>::iterator v_it = values.begin();
+//     val* v = *v_it;
+// 
+// 
+//     ofstream ff1;
+//     int size1 =sizeof(val*);
+//     ff1.open("ca16_v.dat",ios::binary|ios::out); 
+// 
+//     for(int j=0;j<traceSize;j++){
+//         ff1.write((char *)&v[j],size1);
+//     }
+// 
+//     ff1.close();
+    
+    
+    //##################
+    
+    
+    
+    
+//     getKeysAndWeightsFromFile("../../datasets/CAIDA18/mergedAggregatedPktlen_Srcip", keys, values, CAIDA18_SIZE);
+//     sizes.push_back(CAIDA18_SIZE);
+//     datasets.push_back("caida18");
+//     
+//     
+//     vector<key*>::iterator k_it = keys.begin();
+//     key* k = *k_it;
+// 
+//     int size =sizeof(key*);
+//     int traceSize = CAIDA18_SIZE;
+//     ofstream f1;
+// 
+//     f1.open("ca18_k.dat",ios::binary|ios::out); 
+// 
+//     for(int j=0;j<traceSize;j++){
+//         f1.write((char *)&k[j],size);
+//     }
+// 
+//     f1.close();
+//     
+//     
+//     
+//     
+//     vector<val*>::iterator v_it = values.begin();
+//     val* v = *v_it;
+// 
+// 
+//     ofstream ff1;
+//     int size1 =sizeof(val*);
+//     ff1.open("ca18_v.dat",ios::binary|ios::out); 
+// 
+//     for(int j=0;j<traceSize;j++){
+//         ff1.write((char *)&v[j],size1);
+//     }
+// 
+//     ff1.close();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    vector<string> datasets;
+    datasets.push_back("univ1");
+    datasets.push_back("caida16");
+    datasets.push_back("caida18");
+    vector<int> sizes;
+    sizes.push_back(UNIV1_SIZE);
+    sizes.push_back(CAIDA16_SIZE);
+    sizes.push_back(CAIDA16_SIZE);
+        
+    
+    
+    
+    
+    
+    
+    ifstream ff_k;
+    ifstream ff_v;
+    key univ1_k[UNIV1_SIZE]= {};
+    val univ1_v[UNIV1_SIZE] = {};
+    ff_k.open("univ1_k.dat",ios::binary|ios::in);
+    ff_v.open("univ1_v.dat",ios::binary|ios::in);
+
+    for(int i=0;i<UNIV1_SIZE;i++){
+
+        ff_k.read((char *)&univ1_k[i],sizeof(key*));
+        ff_v.read((char *)&univ1_v[i],sizeof(key*));
+
+    }
+    
+    ff_k.close();
+    ff_v.close();
+    
+    
+    
+    
+    
+    
+    
+    
+    ifstream ff1_k;
+    ifstream ff1_v;
+    key ca16_k[CAIDA16_SIZE]= {};
+    val ca16_v[CAIDA16_SIZE] = {};
+    ff1_k.open("ca16_k.dat",ios::binary|ios::in);
+    ff1_v.open("ca16_v.dat",ios::binary|ios::in);
+
+    for(int i=0;i<CAIDA16_SIZE;i++){
+        ff1_k.read((char *)&ca16_k[i],sizeof(key*));
+        ff1_v.read((char *)&ca16_v[i],sizeof(key*));
+
+    }
+    
+    ff1_k.close();
+    ff1_v.close();
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    ifstream ff2_k;
+    ifstream ff2_v;
+    key ca18_k[CAIDA18_SIZE]= {};
+    val ca18_v[CAIDA18_SIZE] = {};
+    ff2_k.open("ca18_k.dat",ios::binary|ios::in);
+    ff2_v.open("ca18_v.dat",ios::binary|ios::in);
+
+    for(int i=0;i<CAIDA18_SIZE;i++){
+
+        ff2_k.read((char *)&ca18_k[i],sizeof(key*));
+        ff2_v.read((char *)&ca18_v[i],sizeof(key*));
+
+    }
+    
+    ff2_k.close();
+    ff2_v.close();
+    
+    
+    
+    vector<key*> keys = {univ1_k, ca16_k, ca18_k};
+    vector<val*> values = {univ1_v, ca16_v, ca18_v};
+
     
 
 
-    getKeysAndWeightsFromFile("../../datasets/CAIDA16/mergedAggregatedPktlen_Srcip", keys, values, CAIDA16_SIZE);
-    sizes.push_back(CAIDA16_SIZE);
-    datasets.push_back("caida");
-
-/*
-
-    getKeysAndWeightsFromFile("../../datasets/CAIDA18/mergedAggregatedPktlen_Srcip", keys, values, CAIDA18_SIZE);
-    sizes.push_back(CAIDA18_SIZE);
-    datasets.push_back("caida18");*/
 
     
     
     
     int k = 1;
-    double maxl = 0.6;
+    double maxl = 0.5;
     double gamma = 1.0;
     
     ofstream ostream;
@@ -143,7 +387,7 @@ int main(int argc, char* argv[])
         vector<int>::iterator s_it = sizes.begin();
         vector<string>::iterator d_it = datasets.begin();
         
-        for (int trc = 0; trc < 1; trc++) {
+        for (int trc = 0; trc < 3; trc++) {
             key* kk = *k_it;
             val* vv = *v_it;
             int size = *s_it;
@@ -190,12 +434,12 @@ int main(int argc, char* argv[])
             
             ostream << "Cuckoo_MC" << dataset<<   "     " << q <<  "    " << time << endl;
 //             ostream1 << "Cuckoo_MC" << dataset<<  "     " << q << "     " << nrmse << endl;
-//             
-//             
-//             ++k_it;
-//             ++v_it;
-//             ++s_it;
-//             ++d_it;
+            
+            
+            ++k_it;
+            ++v_it;
+            ++s_it;
+            ++d_it;
             
         }
         
